f0dde9b00f033f6489518dc68f6469f59547f3a9
Opyd <danielopyd99@gmail.com>
2022-11-06 18:09:38 +0100

    moved to nuxt3

---
+++ frontend/app.vue
@@ -0,0 +1,9 @@
+<script setup>
+import InputSwitch from 'primevue/inputswitch';
+</script>
+
+<template>
+  <NuxtLayout>
+    
+  </NuxtLayout>
+</template>
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  ssr: false,
+css: [
+  'primevue/resources/themes/saga-blue/theme.css',
+  'primevue/resources/primevue.css',
+  'primeicons/primeicons.css'
+],
+build: {
+transpile: ['primevue']
+}
+})
